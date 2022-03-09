1ac9af0a925e4a181afcb084929d0cb31f48efcb
ChandruKumar1994 <44518363+ChandruKumar1994@users.noreply.github.com>
2022-03-09 15:10:40 +0530

    Add files via upload

---
+++ RestAssuredTesting/src/test/java/com/testing/GetcallValidation.java
@@ -0,0 +1,38 @@
+package com.testing;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+
+public class GetcallValidation {
+
+	@Test
+	public void getReg(){
+		//given().when().get("https://api.dictionaryapi.dev/api/v2/entries/en/hello").then().assertThat().statusCode(200);	
+		RestAssured.baseURI = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+		String URI = RestAssured.baseURI;
+		Response res1 = RestAssured.given().when().get(URI + "friend");
+		System.out.println("Response status is "+ res1.getStatusCode());
+
+		if (res1.getStatusCode() == 200) {		
+			res1.then().assertThat().body(matchesJsonSchemaInClasspath("testSchema.json"));
+			System.out.println("Schema Validation is successful");
+			res1.prettyPrint();
+		}
+		else{
+			System.out.println(res1.getStatusCode());
+			Assert.fail("Please give the correct value");
+			
+		}
+
+
+	}
+
+}
